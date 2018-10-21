cc7fc34ee6f96d2773c01e23930e2ffe8bcc098e
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-21 22:47:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,14 @@
 class Dog 
   def initialize(name, breed)
     @name = name
-    @breed = breed
+    @breed = breed 
+  end 
+  
+  def name
+    @name 
+  end 
+  
+  def name=(new_name)
+    @name = new_name
+  end 
 end 
